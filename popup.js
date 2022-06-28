54ee83d9b6458fff0436a32f42a76e6de8d85926
mtake986 <tak21011@byui.edu>
2022-06-27 19:51:25 -0600

    form

--- popup.js
+++ popup.js
@@ -3,3 +3,20 @@ let changeColor = document.getElementById("changeColor");
 chrome.storage.sync.get("color", ({color}) => {
     changeColor.style.backgroundColor = color;
 })
+
+const createBtn = document.getElementById('create-btn');
+const eventForm = document.getElementById('eventForm');
+const eventList = document.getElementById('eventList');
+
+createBtn.addEventListener('click', (e) => {
+    eventForm.classList.toggle("show")
+    eventList.classList.toggle("show")
+
+    createBtn.classList.toggle("btn-outline-danger")
+    // if (e.target.textContent === "Create") {
+    //     e.target.textContent = "Close"
+    // } else {
+    //     e.target.textContent = "Create"
+    // }
+    e.target.textContent = (e.target.textContent === "Create") ? "Close" : "Create"
+});
